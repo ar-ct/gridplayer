be71e0ea74a781884d50000dbb3736918c71a94f
ar-ct <155827809+ar-ct@users.noreply.github.com>
2026-08-18 03:33:04 +0200

    Compare applied and desired sharpen states on dialog close

--- gridplayer/player/managers/settings.py
+++ gridplayer/player/managers/settings.py
@@ -73,17 +73,18 @@ class SettingsManager(ManagerBase):
         self.reload_video_filters.emit()
 
     def _finish_sharpen_preview(self, result, previous_settings, final_value):
-        preview_value = self._sharpen_preview_value
         original_value = float(previous_settings[SHARPEN_SETTING])
+        applied_value = (
+            original_value
+            if self._sharpen_preview_value is None
+            else self._sharpen_preview_value
+        )
+        desired_value = final_value if result == QDialog.Accepted else original_value
+
         set_sharpen_preview(None)
         self._sharpen_preview_value = None
 
-        if result == QDialog.Accepted:
-            needs_refresh = final_value != original_value and preview_value != final_value
-        else:
-            needs_refresh = preview_value is not None
-
-        if needs_refresh:
+        if applied_value != desired_value:
             self.reload_video_filters.emit()
 
     def _apply_settings(self, previous_settings):
