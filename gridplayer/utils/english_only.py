6bdd5c5b7d391d8811645a041c8495788019be6d
ar-ct <155827809+ar-ct@users.noreply.github.com>
2026-09-12 05:31:33 +0200

    Keep removed language page lifecycle-safe

--- gridplayer/utils/english_only.py
+++ gridplayer/utils/english_only.py
@@ -5,8 +5,6 @@ def remove_language_settings_ui(dialog) -> None:
     language_page = dialog.page_general_language
     dialog.section_page.removeWidget(language_page)
     language_page.hide()
-    language_page.setParent(None)
-    language_page.deleteLater()
 
     for row in range(dialog.section_index.count()):
         item = dialog.section_index.item(row)
